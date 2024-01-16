085d9d155bbfbb0c80cf4ab9931a57fa67ea0028
igordudka <i.dudka007@gmail.com>
2024-01-16 23:02:44 +0300

    battle

--- Game.py
+++ Game.py
@@ -2,30 +2,10 @@ import pygame
 import os
 import sys
 import random
+import time
 
 
         
-pygame.init()
-pygame.mixer.init()
-music_list = ["./data/Summer.mp3", "./data/Autumn.mp3", "./data/Winter.mp3",
-              "./data/zemlya-v-illyuminatore.mp3"]
-pygame.mixer.music.load("./data/Summer.mp3")
-size = width, height = 500, 500
-screen = pygame.display.set_mode(size)
-grass = pygame.sprite.Group()
-trees = pygame.sprite.Group()
-stars = pygame.sprite.Group()
-Hero = pygame.sprite.Group()
-maps_list = []
-f = open("./data/maps_list.txt", mode="rt", encoding="utf-8")
-for number, line in enumerate(f):
-    maps_list.append(line[:-1])
-f.close()
-location = 0
-screen_rect = (0, 0, width, height)
-
-
-
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -41,6 +21,37 @@ def load_image(name, colorkey=None):
     else:
         image = image.convert_alpha()
     return image
+
+
+pygame.init()
+pygame.mixer.init()
+music_list = ["./data/Summer.mp3", "./data/Autumn.mp3", "./data/Winter.mp3",
+              "./data/zemlya-v-illyuminatore.mp3"]
+pygame.mixer.music.load("./data/Summer.mp3")
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+grass = pygame.sprite.Group()
+trees = pygame.sprite.Group()
+stars = pygame.sprite.Group()
+Hero = pygame.sprite.Group()
+monsters = pygame.sprite.Group()
+bosses = pygame.sprite.Group()
+enemies_hp = [100, 200, 100, 200, 100, 200, 100, 200]
+hero_hp = 500
+green = (0, 255, 0)
+maps_list = []
+f = open("./data/maps_list.txt", mode="rt", encoding="utf-8")
+for number, line in enumerate(f):
+    maps_list.append(line[:-1])
+f.close()
+location = 0
+screen_rect = (0, 0, width, height)
+monster_spr = pygame.transform.scale(load_image("monster_1.png"), (50, 80))
+boss_spr = pygame.transform.scale(load_image("boss_monster.png"), (50, 80))
+
+
+attack_timer = time.time()
+attack_interval = 1
 
 
 def terminate():
@@ -112,6 +123,45 @@ def win_screen():
                 terminate()
 
 
+def lose_screen():
+
+    text = ["Вы проиграли...(( Нажмите L"]
+    fon = pygame.transform.scale(load_image('main_screen.jpeg'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    string_rendered = font.render(text[0], 1, pygame.Color('black'))
+    intro_rect = string_rendered.get_rect()
+    text_coord = 250
+    intro_rect.top = text_coord
+    intro_rect.x = 150
+    
+    while True:
+        
+        stars.update()
+        screen.fill((0, 0, 0))
+        screen.blit(fon, (0, 0))
+        for line in text:
+            screen.blit(string_rendered, intro_rect)
+        stars.draw(screen)    
+        clock.tick(50)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                resetAll()
+                return 
+        pygame.display.flip()
+
+
+def resetAll():
+    global location
+    global hero_hp    
+    global enemies_hp 
+    location = 0
+    hero_hp = 500
+    enemies_hp = [100, 200, 100, 200, 100, 200, 100, 200]
+
 def new_location():
     global location
     location += 1
@@ -152,6 +202,7 @@ class Particle(pygame.sprite.Sprite):
         self.rect.y += self.velocity[1]
         if not self.rect.colliderect(screen_rect):
             self.kill()
+
     
         
 
@@ -192,16 +243,30 @@ class Board:
                     sprite.rect.x = 25 + 100 * j
                     sprite.rect.y = 10 + 100 * i
                     trees.add(sprite)
+                if self.map[i][j].isdigit() and int(self.map[i][j]) % 2 == 0:
+                    monster = Monster(enemies_hp[int(self.map[i][j])], 25 + 100 * j, 10 + 100 * i)
+                    monsters.add(monster)
+                if self.map[i][j].isdigit() and int(self.map[i][j]) % 2 != 0:
+                    boss = Monster(enemies_hp[int(self.map[i][j])], 25 + 100 * j, 10 + 100 * i, True)
+                    monsters.add(boss)
 
     def can_move(self, x, y):
         if self.map[y][x] == "_":
             return True
-        elif self.map[y][x] == "t":
+        else:
             return False
+    
+    def free_tile(self, num):
+        for i in range(len(self.map)):
+            if str(num) in self.map[i]:
+                self.map[i][self.map[i].index(str(num))] = "_"
+                break
     
     def render(self, screen):
         grass.draw(self.screen)
-        trees.draw(self.screen)        
+        trees.draw(self.screen) 
+        monsters.draw(self.screen)    
+        bosses.draw(self.screen)   
 
 
 class Camera:
@@ -272,6 +337,22 @@ class M_Hero(pygame.sprite.Sprite):
         self.cam_y = 0
         self.b = b
         self.loc = 1
+    
+    def reset_coords(self):
+        self.coords = [0, 0]
+
+    def draw_health_bar(self, screen):
+        health_bar_width = 50
+        health_bar_height = 5
+        background_rect = pygame.Rect(
+            self.rect.x + 50, self.rect.y - 10, health_bar_width, health_bar_height
+        )
+        pygame.draw.rect(screen, (255, 0, 0), background_rect)
+        current_health_width = (hero_hp / 500) * health_bar_width
+        current_health_rect = pygame.Rect(
+            self.rect.x + 50, self.rect.y - 10, current_health_width, health_bar_height
+        )
+        pygame.draw.rect(screen, (0, 255, 0), current_health_rect)
 
     def up(self):
         self.n_coords = [self.coords[0], self.coords[1] - 1]
@@ -428,8 +509,36 @@ class M_Hero(pygame.sprite.Sprite):
         self.coords = [0, 0]
         self.b = b_new
         Hero.draw(screen)
+
         
-    
+class Monster(pygame.sprite.Sprite):
+    def __init__(self, health, x, y, isBoss=False):
+        super().__init__()
+        if isBoss:
+            self.image = boss_spr
+            self.max = 200
+        else:
+            self.image = monster_spr
+            self.max = 100
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.health = health
+
+
+    def draw_health_bar(self, screen, hp):
+        health_bar_width = 50
+        health_bar_height = 5
+        background_rect = pygame.Rect(
+            self.rect.x, self.rect.y - 10, health_bar_width, health_bar_height
+        )
+        pygame.draw.rect(screen, (255, 0, 0), background_rect)
+        current_health_width = (hp / self.max) * health_bar_width
+        current_health_rect = pygame.Rect(
+            self.rect.x , self.rect.y - 10, current_health_width, health_bar_height
+        )
+        pygame.draw.rect(screen, (0, 255, 0), current_health_rect)
+
                         
 
 
@@ -441,6 +550,8 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     start_screen()
     pygame.mixer.music.play(-1)
+    enemy_num = 0
+    enemy = Monster(100, 0, 0)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -454,7 +565,30 @@ if __name__ == '__main__':
                     hero.up()
                 elif event.key == pygame.K_DOWN:
                     hero.down()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                enemies_hp[enemy_num] -= 10
+        if enemies_hp[enemy_num] <= 0:
+            monsters.remove(enemy)
+            board.free_tile(enemy_num)
+            enemy_num += 1
+        if hero_hp == 0:
+            hero.reset_coords()
+            lose_screen()
+        for monster in monsters:
+            distance = pygame.math.Vector2(monster.rect.x - hero.rect.x, monster.rect.y - hero.rect.y).length()
+            if distance <= 150:
+                enemy = monster
+                if time.time() - attack_timer >= attack_interval:
+                    hero_hp -= 10
+                    attack_timer = time.time()
+
         board.render(screen)
         Hero.draw(screen)
+        hero.draw_health_bar(screen)
+        counter = 0
+        for monster in monsters:
+            monster.draw_health_bar(screen, enemies_hp[counter])
+            counter += 1
         pygame.display.flip()
+        clock.tick(60)
 
